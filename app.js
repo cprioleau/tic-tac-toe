93aadde676c5a30192c5ce969055d89c9f6d3035
Caroline Prioleau <caroline@prioleau.com>
2016-11-10 19:19:48 -0800

    second commit

--- app.js
+++ app.js
@@ -1,19 +1,27 @@
 $(document).ready(function () {
 
+// didn't really need to set variable $boxes since only called once
   var $boxes = $(".boxes");
-  var $flag = 0;
+  var flag = 0;
 
+// check to see if the html is empty
+// if ($(this).html()===""){}
+// console.log($(this).html(), "letter selected");
+
+// click function on .boxes
   $boxes.on("click", function() {
+    // check to see if the box has been marked done or not
     if (!$(this).hasClass("done")) {
-      if ($flag === 0) {
+      // add the html and flip the turn or flag
+      if (flag === 0) {
         $(this).html("X");
-        $flag = 1;
-        $(this).addClass("done");
+        flag = 1;
       } else {
         $(this).html("O");
-        $flag = 0;
-        $(this).addClass("done");
+        flag = 0;
       }
+      // mark the box as done
+      $(this).addClass("done");
     }
   });
 
